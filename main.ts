fa0a8e4f809b2b528c1710d7b3f6a202e91b9452
Carlo Lobrano <c.lobrano@gmail.com>
2023-02-12 17:47:15 +0100

    Set Hamster's metadata from file metadata
    If the task description does not already have a project/context set
    (a word starting with @ symbol), the plugin will use a metadata field in
    frontmatter named "project".
    
    If the file frontmatter also has tags, those will be added as well.

--- main.ts
+++ main.ts
@@ -20,7 +20,7 @@ export default class ObsidianForHamster extends Plugin {
 		this.addCommand({
 			id: 'start-hamster-timer',
 			name: 'Start Hamster timer',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
 				if (!this.hamster) {
 					this.hamsterConnect();
 					if (!this.hamster) {
@@ -28,13 +28,12 @@ export default class ObsidianForHamster extends Plugin {
 						return;
 					}
 				}
-				let cursor = editor.getCursor();
-				let line = editor.getLine(cursor.line);
-				if (!this.isTask(line)) {
-					new Notice("This is not an actionable item");
-					return;
-				}
-				let task = this.sanitize(line);
+				let file = app.workspace.getActiveFile();
+				let metadata = await this.getFrontmatterMetadata(file);
+				let line = this.getCurrentLine(editor);
+				let task = this.composeTask(line, metadata);
+				if (!task)
+					return
 				this.hamster.AddFact(task, 0, 0, false);
 			}
 		});
@@ -110,10 +109,65 @@ export default class ObsidianForHamster extends Plugin {
 		const parsedYaml = parseYaml(yamlContent);
 
 		for (const key in parsedYaml) {
-			console.log(key + ": " + parsedYaml[key])
 			metadata[key] = parsedYaml[key]
 		}
 		return metadata
+	}
+
+	getCurrentLine(editor: Editor) {
+		let cursor = editor.getCursor();
+		return editor.getLine(cursor.line);
+	}
+
+	composeTask(line: string, metadata) {
+		if (!this.isTask(line)) {
+			new Notice("This is not an actionable item");
+			return null;
+		}
+
+		let task = this.sanitize(line);
+
+		if (!task.includes("@") && metadata["project"]) {
+			let project = metadata["project"]
+			if (task.includes(",,")) {
+				// project goes before ",,"
+				const parts = task.split(",,")
+				task = parts[0] + "@" + project + ",," + parts[1]
+			} else {
+				task += "@" + project;
+			}
+		}
+
+		if (metadata["tags"]) {
+			let tags = this.getTagsFromMetadata(metadata)
+			console.log(tags);
+			if (!task.includes(",,")) {
+				task += ",,"
+			}
+			task += tags;
+		}
+		console.log(task)
+		return task;
+	}
+
+	getTagsFromMetadata(metadata) {
+		let tags = ""
+
+		if (!metadata["tags"]) {
+			return tags;
+		}
+		let parts = []
+		if (Array.isArray(metadata["tags"])) {
+			parts = metadata["tags"];
+		} else {
+			parts = metadata["tags"].split(",")
+		}
+		
+		for(const i in parts) {
+			tags += " #" + parts[i].trim() + ",";
+		}
+		
+		return tags;
 	}
 }
 
